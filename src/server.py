da614a156de3b4c2fc4b0a2038a052548416c493
Eric Arellano <ericarellano@me.com>
2019-07-03 15:26:25 -0700

    Create the Identifier data model

--- src/server.py
+++ src/server.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from __future__ import annotations
+
+import dataclasses
+from typing import Any, Dict, Optional
+
+import flask
 from google.cloud import iam_credentials_v1
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 credentials_client = iam_credentials_v1.IAMCredentialsClient()
 
 rbe_execute_tests_scope = [
@@ -13,6 +18,7 @@ rbe_execute_tests_scope = [
     "remotebuildexecution.logstreams.get",
     "remotebuildexecution.logstreams.update",
 ]
+
 # TODO: the service_account credentials.
 #  https://googleapis.github.io/google-cloud-python/latest/iam/gapic/v1/api.html
 service_account_path = credentials_client.service_account_path(
@@ -20,8 +26,34 @@ service_account_path = credentials_client.service_account_path(
 )
 
 
-@app.route("/")
+@dataclasses.dataclass(frozen=True)
+class Identifier:
+    commit_sha: str
+    travis_job_id: str
+    branch_name: Optional[str] = None
+    pr_number: Optional[int] = None
+
+    def __post_init__(self) -> None:
+        if self.branch_name is None and self.pr_number is None:
+            raise ValueError(
+                "Must specify either the branch name (for branch builds) "
+                "or the PR number (for PR builds)."
+            )
+
+    @staticmethod
+    def from_json(json: Dict[str, Any]) -> Identifier:
+        return Identifier(
+            commit_sha=json["commit_sha"],
+            travis_job_id=json["travis_job_id"],
+            branch_name=json["branch_name"] if "branch_name" in json else None,
+            pr_number=int(json["pr_number"]) if "pr_number" in json else None,
+        )
+
+
+@app.route("/token/generate", methods=["POST"])
 def generate_token() -> str:
-    return credentials_client.generate_access_token(
-        name=service_account_path, scope=rbe_execute_tests_scope
-    ).access_token
+    identifier = Identifier.from_json(flask.request.get_json())
+    return str(identifier)
+    # return credentials_client.generate_access_token(
+    #     name=service_account_path, scope=rbe_execute_tests_scope
+    # ).access_token
